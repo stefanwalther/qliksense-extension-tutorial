2018a014a2dfff98fd3e465560d5401c0e4481f1
Stefan Walther <swr@qlik.com>
2015-02-22 00:18:09 +0100

    First very simple deployment system using assemble.io

---
+++ docgen/Gruntfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = function ( grunt ) {
+
+	var config = grunt.file.readYAML( './config.yml' );
+
+	grunt.initConfig( {
+
+		assemble: {
+			project: {
+				options: {
+					trace: true,
+					layoutdir: config.assemble.layoutdir,
+					layout: config.assemble.layout,
+					ext: '.md',
+					data: './config.yml'
+					//partials: 'templates/partials/**/*.hbs',
+					//helpers: 'templates/helpers/**/*.hbs',
+
+				},
+				files: [{
+					flatten: false,
+					expand: true,
+					cwd: './../docs/',
+					src: ['**/*.md'],
+					dest: './../tutorial/'
+				}]
+			}
+		}
+
+	} );
+
+	grunt.loadNpmTasks( 'assemble' );
+	grunt.registerTask( 'default', ['assemble'] );
+
+};
+
