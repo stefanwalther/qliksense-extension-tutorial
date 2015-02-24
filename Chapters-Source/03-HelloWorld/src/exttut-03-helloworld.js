93807f7b0b4a7523e7343e8dc5e497bf75892f35
Stefan Walther <swr@qlik.com>
2015-02-24 19:45:22 +0100

    Source for Chapter 03

--- Chapters-Source/03-HelloWorld/src/exttut-03-helloworld.js
+++ Chapters-Source/03-HelloWorld/src/exttut-03-helloworld.js
@@ -8,10 +8,36 @@ define( [
 
 			paint: function ( $element, layout ) {
 
-				$element.empty();
 				var $helloWorld = $( document.createElement( 'div' ) );
-				$helloWorld.html( 'Hello World from the extension "01-ExtTut-HelloWorld"' );
+				$helloWorld.html( 'Hello World from the extension "01-ExtTut-HelloWorld"<br/>' );
 				$element.append( $helloWorld );
+
+				// ****************************************************************************************
+				// Solve the "multiply problem" - Lazy way
+				// (uncomment the following lines if you want to test it)
+				// ****************************************************************************************
+				//$element.empty();
+				//var $helloWorld = $( document.createElement( 'div' ) );
+				//$helloWorld.attr( 'id', 'MyId');
+				//$helloWorld.html( 'Hello World' );
+				//$element.append( $helloWorld );
+
+				// ****************************************************************************************
+				// Solve the "multiply problem" - The smarter way
+				// (uncomment the following lines if you want to test it)
+				// ****************************************************************************************
+				//var id = layout.qInfo.qId + '_helloworld';
+				//var $helloWorld = $( '#' + id );
+				//if ( !$helloWorld.length ) {
+				//	console.log( 'No element found with the given Id, so create the element' );
+				//	$helloWorld = $( document.createElement( 'div' ) );
+				//	$helloWorld.attr( 'id', id );
+				//	$helloWorld.html( 'Hello World' );
+				//	$element.append( $helloWorld );
+				//} else {
+				//	console.log( 'Found an element with the given Id, so just change it' );
+				//	$helloWorld.html( 'Hello World' );
+				//}
 
 			}
 		};
